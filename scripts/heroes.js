673da7580e1d2d5a566a714a5027075b4f8829b2
Dawid Grygowicz <grygowiczdawid@gmail.com>
2022-05-16 11:34:55 +0200

    added heroes filtering by factions and some minor changes

--- scripts/heroes.js
+++ scripts/heroes.js
@@ -1,4 +1,38 @@
+const filteredFactions = [];
 class Heroes {
+  static filterHeroes(event) {
+    let factionElement = event.target.parentElement;
+
+    if (filteredFactions.includes(event.target.alt)) {
+      let index = filteredFactions.indexOf(event.target.alt);
+      filteredFactions.splice(index, 1);
+      factionElement.classList.remove('factions-checked');
+    } else {
+      filteredFactions.push(event.target.alt);
+      factionElement.classList.add('factions-checked');
+    }
+    const filteredHeroes = heroesList.filter(hero => {
+      return filteredFactions.every(faction => {
+        return hero.factions.includes(faction);
+      });
+    });
+    const heroElements = document.querySelectorAll('#heroes-cards li');
+    if (filteredHeroes.length > 0) {
+      heroElements.forEach(el => {
+        if (
+          filteredFactions.length === 0 ||
+          el.getAttribute('id') === filteredHeroes[0].name
+        ) {
+          el.classList.remove('visibility');
+        } else {
+          el.classList.add('visibility');
+        }
+      });
+    } else {
+      heroElements.forEach(el => el.classList.add('visibility'));
+    }
+  }
+
   static renderFactions() {
     factionsList.sort((a, b) =>
       a.name > b.name ? 1 : b.name > a.name ? -1 : 0
@@ -8,48 +42,29 @@ class Heroes {
       const factionElement = document.createElement('li');
       const factionName = faction.name;
       const factionImage = faction.imageName;
-
       factionElement.innerHTML = `
         <img src="/images/factions/${factionImage}" alt="${factionName}"/>`;
 
-      //TODO filtering by factions
-      factionElement.addEventListener('click', () => {
-        console.log(factionElement);
-      });
+      factionElement.addEventListener('click', Heroes.filterHeroes);
       factionCards.appendChild(factionElement);
     }
   }
 
   static renderHeroes() {
     heroesList.sort((a, b) => (a.name > b.name ? 1 : b.name > a.name ? -1 : 0));
-    // for (const hero of heroesList) {
-    //   const heroCards = document.getElementById('heroes-cards');
-    //   const heroElement = document.createElement('li');
-    //   const heroName = hero.name;
-    //   const heroImage = hero.cardName;
-    //   heroElement.className = 'hero-card';
-
-    //   heroElement.innerHTML = `
-    //   <a href="/heroes/${heroName.toLowerCase()}.html">
-    //   <img src="/images/heroes_cards/${heroImage}"/>
-    // </a>
-    // <p>${heroName}</p>`;
-    //   heroCards.appendChild(heroElement);
-    // }
-
-    // temporary solution when no more hero data
-    for (let i = 0; i < 50; i++) {
+    for (const hero of heroesList) {
       const heroCards = document.getElementById('heroes-cards');
       const heroElement = document.createElement('li');
-      const heroName = heroesList[0].name;
-      const heroImage = heroesList[0].cardName;
+      const heroName = hero.name;
+      const heroImage = hero.cardName;
+      heroElement.setAttribute('id', `${heroName}`);
       heroElement.className = 'hero-card';
 
       heroElement.innerHTML = `
       <a href="/heroes/${heroName.toLowerCase()}.html">
-        <img src="/images/heroes_cards/${heroImage}"/>
-      </a>
-      <p>${heroName}</p>`;
+      <img src="/images/heroes_cards/${heroImage}"/>
+    </a>
+    <p>${heroName}</p>`;
       heroCards.appendChild(heroElement);
     }
   }
